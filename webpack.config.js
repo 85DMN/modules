446c70d634753dc75a72db6ff49faad451432bf3
Danilchenko Mihail <mihaildanilchenko85@gmail.com>
2026-03-08 20:37:40 +0300

    rebuild all project

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
-const path = require('path');
+const path = require('path')
+const HtmlWebpackPligin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'result.js'
+    entry: {
+        main: path.resolve(__dirname, './src/app.js'),
     },
-
-};
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test:/\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPligin({
+            template: './src/template.html'
+        })
+    ]
+    
+}
